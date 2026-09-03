74f5fddd109e9a8d35142c40ede98e1bcd3b76ba
OM-Coder95 <ommore8855@gmail.com>
2026-09-03 18:51:55 +0530

    Implement Todo Create functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,19 @@
 const cl = console.log;
 
 const todocontainer = document.getElementById("todocontainer");
+const form = document.getElementById("form");
+const taskName = document.getElementById("taskName");
+const priority = document.getElementById("priority");
 
 let jsonArr = localStorage.getItem("todoArr");
 
 let todoArr = jsonArr ? JSON.parse(jsonArr) : [];
 
+// SaveData
+
+function saveTodos() {
+  localStorage.setItem("todoArr", JSON.stringify(todoArr));
+}
 // Read
 
 function showOnUI(arr) {
@@ -28,3 +36,36 @@ function showOnUI(arr) {
 }
 
 showOnUI(todoArr);
+
+// Create
+
+function onTodoAdd(event) {
+  event.preventDefault();
+
+  let newTodo = {
+    id: crypto.randomUUID(),
+    taskName: taskName.value.trim(),
+    priority: priority.value.trim(),
+  };
+
+  todoArr.push(newTodo);
+  saveTodos();
+
+  let tr = document.createElement("tr");
+
+  tr.id = newTodo.id;
+
+  tr.innerHTML = `
+                                    <td>${todoArr.length}</td>
+                                    <td>${newTodo.taskName}</td>
+                                    <td>${newTodo.priority}</td>
+                                    <td>
+                                        <i class="fa-regular fa-pen-to-square fa-2x text-primary"></i>
+                                        <i class="fa-solid fa-trash-can fa-2x text-danger"></i>
+                                    </td>
+  `;
+
+  todocontainer.append(tr);
+}
+
+form.addEventListener("submit", onTodoAdd);
